84948803b3c3cd0700b3b2a427312a4ef29a74e4
Sean Pianka <pianka@eml.cc>
2017-12-21 19:05:57 -0500

    Add more descriptive failure output

--- 344I-BankersAlgorithm/bank.py
+++ 344I-BankersAlgorithm/bank.py
@@ -10,8 +10,8 @@ while processes:
         order.append([name for name, state in processes.items() if all([state['max'][i] - state['allocation'][i] <= available[i] for i in range(len(available))])][0])
         available = [available[i] + processes[order[-1]]['allocation'][i] for i in range(len(available))]
         del[processes[order[-1]]]
-    except IndexError:
-        print('No way to complete the algorithm.')
+    except IndexError: # No process was able to be run with available resources
+        print(f'Cannot complete algorithm, the processes <{order}> were able to complete.')
         break
 else:
     print(', '.join(order))
